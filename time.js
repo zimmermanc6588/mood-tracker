f8173ae7df7821df0a10aeb9985dadc1085aa394
jake <jakedlee2018@gmail.com>
2021-07-08 20:22:52 -0500

    added time and date display

---
+++ time.js
@@ -0,0 +1,40 @@
+// use getDate getMonth and getYear (months are 0-11)
+// make date show up in window
+// make sure the date can update itself evryday
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let d = today.getDate();
+    let n = today.getMonth();
+    let y = today.getFullYear();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = y + "/" + n + "/" + d + " " + h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+startTime();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
