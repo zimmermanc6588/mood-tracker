23dd81b8466a787f66f9422c6b890e215febe126
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-07-10 13:28:31 -0500

    quote displays

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,6 +1,32 @@
 var apiurl ="https://type.fit/api/quotes";
-fetch(apiurl)
-.then(response => response.json())
-    .then(data => console.log(data));
+// fetch(apiurl)
+// .then(function(response) {
+//     response.json().then(function(data) {
+//       displayQuote()
+      
+//     });
+//   });
+
+
     
 
+async function getQuote() {
+   const response = await fetch(apiurl).then(response => {
+     return response.json()
+   })
+
+   console.log("response", response)
+   // make this random based on lenght of array returned
+   displayQuote(response[0].text);
+
+}
+
+
+
+const newQuoteButton =document.querySelector("#js-new-quote");
+newQuoteButton.addEventListener('click', getQuote);
+
+function displayQuote(quote) {
+    const quoteText =document.querySelector('#js-quote-text');
+    quoteText.textContent=quote;
+}
