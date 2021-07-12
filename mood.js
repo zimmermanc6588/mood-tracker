8c3a8b525a848dd2757278c5060a86e8998f6aa6
zimmermanc6588 <82486396+zimmermanc6588@users.noreply.github.com>
2021-07-11 20:26:25 -0500

    Update mood.js
    initial mood.js

--- mood.js
+++ mood.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
     $('select').formSelect();
   });
         
+  
+let userInput = [];
 
+let saveBtnEl = document.getElementById("save-mood");
+
+function storeUserInput(event) {
+    event.preventDefault();
+    
+    let dropdownEl = document.getElementById("mood").value;
+    let textInputEl = document.getElementById("activity").value;
+    
+    userInput.push(dropdownEl, textInputEl);
+    localStorage.setItem("userInput", JSON.stringify(userInput));
+    let userData = (JSON.parse(localStorage.getItem("userInput")));
+    let moodListItem = document.createElement("li");
+    moodListItem.innerHTML = userData;
+    document.getElementById("moods-list").appendChild(moodListItem);
+}
+
+saveBtnEl.addEventListener("click", storeUserInput);
