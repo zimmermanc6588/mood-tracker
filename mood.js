f8212031a25a0960771be0f085997b53cafe8ffa
Thomas Bewell <tombewell@gmail.com>
2021-07-12 19:22:44 -0500

    fixed mood list display

--- mood.js
+++ mood.js
@@ -3,17 +3,17 @@ let userInput = [];
 let saveBtnEl = document.getElementById("save-mood");
 
 function storeUserInput(event) {
-    event.preventDefault();
+  event.preventDefault();
     
-    let dropdownEl = document.getElementById("mood").value;
-    let textInputEl = document.getElementById("activity").value;
-    
-    userInput.push(dropdownEl, textInputEl);
-    localStorage.setItem("userInput", JSON.stringify(userInput));
-    let userData = (JSON.parse(localStorage.getItem("userInput")));
-    let moodListItem = document.createElement("li");
-    moodListItem.innerHTML = userData;
-    document.getElementById("moods-list").appendChild(moodListItem);
+  let dropdownEl = document.getElementById("mood").value;
+  let textInputEl = document.getElementById("activity").value;
+  
+  userInput.push(dropdownEl, textInputEl);
+  localStorage.setItem("userInput", JSON.stringify(userInput));
+  let userData = (JSON.parse(localStorage.getItem("userInput")));
+  let moodListItem = document.createElement("li");
+  moodListItem.innerHTML = userData.slice(-2);
+  document.getElementById("moods-list").appendChild(moodListItem);
 }
 
 saveBtnEl.addEventListener("click", storeUserInput);
