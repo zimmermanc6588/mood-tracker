ecc1a4782a9056b85cf032dd781081ca4fecf6fa
maris <marisandb@yahoo.com>
2021-07-13 23:09:14 -0500

    fixed a bug where pulled data was not being displayed correctly

--- assets/js/mood.js
+++ assets/js/mood.js
@@ -1,9 +1,8 @@
-
 $(document).ready(function(){
-    $('select').formSelect();
-  });
-        
-  
+  $('select').formSelect();
+});
+      
+
 
 let userInput = [];
 
@@ -16,23 +15,16 @@ function storeUserInput(event) {
   let dropdownEl = document.getElementById("mood").value;
   let textInputEl = document.getElementById("activity").value;
   
-  userInput.push(dropdownEl + ", " + textInputEl);
+  userInput.push(dropdownEl, textInputEl);
   localStorage.setItem("userInput", JSON.stringify(userInput));
   let userData = (JSON.parse(localStorage.getItem("userInput")));
 
-  let loggedMood = document.createElement("ul");
-  loggedMood.classList = "collection with-header"
-  document.getElementById("moods-list").appendChild(loggedMood)
-  let header = document.createElement("li")
-  header.textContent = "My Logged Moods and Activities"
-  header.classList = "collection-header"
-  loggedMood.appendChild(header);
 
   let moodListItem = document.createElement("li");
   moodListItem.textContent = userData.slice(-2);
   moodListItem.classList = "collection-item"
-  loggedMood.appendChild(moodListItem);
-
+  let header = document.getElementById("list-header")
+  header.appendChild(moodListItem);
 }
 
 saveBtnEl.addEventListener("click", storeUserInput);
